cf3bc3cd8538877b0ec392bde8101ac67e33324f
Jaffar Raza <jaffaraza@gmail.com>
2020-03-20 17:02:53 +0500

    Deals Api

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], funct
 
 Route::get('home', 'Admin\CategoryController@api');
 Route::get('companies', 'CompanyController@api');
+Route::get('deals', 'DealController@api');
  
