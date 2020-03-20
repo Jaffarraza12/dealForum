ec47525fc423db327a3a9ebc938f6169612dbe4c
Jaffar Raza <jaffaraza@gmail.com>
2020-03-21 04:34:22 +0500

    Detail module.

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], funct
 
 Route::get('home', 'Admin\CategoryController@api');
 Route::get('companies', 'CompanyController@api');
-Route::get('deals', 'dealsController@api');
-Route::get('deal-detail', 'dealsController@detailapi');
+Route::get('deals', 'DealController@api');
+Route::get('deal-detail', 'DealController@detailapi');
 
  
