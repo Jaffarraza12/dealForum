2eb21226800c65c971d3cca453b671d9107d16bf
Jaffar Raza <jaffaraza@gmail.com>
2020-06-04 13:06:54 +0500

    TEST

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -28,7 +28,7 @@ class CustomerController extends Controller
         
         if( Customer::where('email',$resp['email'])->count() > 0){
             return response()
-            ->json(Customer::where('email',$resp['email')->get());
+            ->json(Customer::where('email',$resp['email'])->get());
 
         }
 
