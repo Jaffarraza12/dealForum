12941d5cf45774865bc290d23ceab579ca52852c
Jaffar Raza <jaffaraza@gmail.com>
2020-06-16 15:59:32 +0500

    AVG Col

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -182,8 +182,7 @@ class DealController extends Controller
 
      public function api(Request $request){
 
-        $deals = Deal::select('deals.*','companies.name AS company')
-        ->( DB::raw("({select avg(rating.vote) from rating where deal= deals.id->toSql()}) as sub") 
+        $deals = Deal::select('deals.*','companies.name AS company', DB::raw('select avg(rating.vote) from rating where deal= deals.id AS AVG'))
         ->join('companies', 'deals.company_id', '=', 'companies.id')      
         ->where('deals.id','!=',0);
         if($request->get('company') <> ''){
