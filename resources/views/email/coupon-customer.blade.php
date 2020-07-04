0527af2f3f41f5d47571bc85919cec45b3e01d18
Jaffar Raza <jaffaraza@gmail.com>
2020-07-04 14:26:00 +0500

    email formating.

--- resources/views/email/coupon-customer.blade.php
+++ resources/views/email/coupon-customer.blade.php
@@ -1,12 +1,16 @@
-<div style="width: 550px;margin:auto;text-align: center">
+<div style="width: 550px;margin:auto;text-align: center;border:1px solid #ccc;padding: 4px;">
 	<div id="header" style="text-align: center;padding:20px 0;">
 		<img src="http://deal-forum.com/asset/dealforum.png" width="200" height="auto" />
 		<p style="text-align: center;">Hello <i>{{$name}}</i></p>
 	</div>	
 	<div style="background: #842d64;padding:40px 0;text-align: center;; ">
-			<h1 style="color:'#fff">SAVE {{$discount}}% on {{$deal}}</h1>
-			 <p style="text-align: center;;">using</p> 
-			<h3 style="color:'#fff">{{$coupon}}</h3> 
+			<h1 style="    color: #fff;
+    text-transform: uppercase;
+    font-size: 45px;>SAVE {{$discount}}% on {{$deal}}</h1>
+			 <p style="text-align:center;color:#fff;">using</p> 
+			<h3 style="text-transform: uppercase;
+    color: white;
+    font-size: 35px;">{{$coupon}}</h3> 
 	</div>
 
 	<div style="background: #fff">
