2918ee0c4fbe331d194cb634afb2fdebd8108c4b
Jaffar Raza <jaffaraza@gmail.com>
2020-08-10 14:06:34 +0500

    auth check

--- app/Http/Controllers/Admin/FileManager.php
+++ app/Http/Controllers/Admin/FileManager.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
 use App;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
 
 class FileManager extends Controller
 {
