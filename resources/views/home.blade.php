ba6ae0bf3de25215d93e9973da6a712b4007be04
jaffar raza <jaffaraza@gmail.com>
2020-09-06 16:59:10 +0500

    Home Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,35 +6,54 @@
 		border:1px solid #0056ad;
 		border-radius:10%;
 		padding:40px;
+		position: relative;
+
+	}
+	.grid h2{
+		    text-transform: uppercase;
+  		  margin-bottom: 18px;
 	}
 	.fa{
 		font-size: 36px;
+		font-size: 51px;
+    	color: #d6d6d6;
+	}
+	.val{
+		    font-size: 48px;
+    position: absolute;
+    right: 45px;
+    bottom: 7px;
+    color:#3f51b5;
 	}
 </style>
-<div class="content">
-    <div class="row" style="min-height:600px;">
+<div class="content" style="min-height:600px;">
+    <div class="row" >
         <div class="col-lg-3">
             <div class="gird">
-            	<div class="pull-left">Companies</div>
-            	<div class="pull-right"><i class="fa fa-building"></i> <span class="val">10</span></div>
+            	<h2>Companies</h2>
+            	<i class="fa fa-building"></i>
+            	<div class="pull-right"> <span class="val">10</span></div>
             </div>
         </div>
          <div class="col-lg-3">
             <div class="gird">
-            	<div class="pull-left">Deals</div>
-            	<div class="pull-right"><i class="fa fa-tag"></i> <span class="val">20</span> </div>
+            	<h2>Deals</h2>
+            	<i class="fa fa-tag"></i> 
+            	<div class="pull-right"><span class="val">20</span> </div>
             </div>
         </div>
          <div class="col-lg-3">
             <div class="gird">
-            	<div class="pull-left">Customer</div>
-            	<div class="pull-right"><i class="fa fa-user"></i> <span class="val">20</span></div>
+            	<h2>Customer</h2>
+            	<i class="fa fa-user"></i>
+            	<div class="pull-right"> <span class="val">20</span></div>
             </div>
         </div>
          <div class="col-lg-3">
             <div class="gird">
-            	<div class="pull-left">Coupons</div>
-            	<div class="pull-right"><i class="fa fa-percent"></i> <span class="val">20</span></div>
+            	<h2>Coupons</h2>
+            	<i class="fa fa-percent"></i> 
+            	<div class="pull-right"><span class="val">20</span></div>
             </div>
         </div>
     </div>
@@ -47,7 +66,7 @@
         </div>
           <div class="col-lg-6">
             <div class="gird">
-            	<h2 >Coupon Consumed</h2>
+            	<h2 >Coupon </h2>
             
             </div>
         </div>
