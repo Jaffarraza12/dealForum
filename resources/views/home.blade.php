8a324e88878aa3d7ed3bb2c7a4f3ed64b224d85d
jaffar raza <jaffaraza@gmail.com>
2020-09-06 16:33:35 +0500

    Home Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.admin')
 @section('content')
 <div class="content">
-    <div class="row" style="min-height:500px;">
+    <div class="row" style="height:90%;">
         <div class="col-lg-3">
             <div class="gird">
             	<div class="pull-left">Companies</div>
-            	<div class="pull-right"></div>
+            	<div class="pull-right"><i class="fa fa-building"></i> 10</div>
             </div>
         </div>
          <div class="col-lg-3">
