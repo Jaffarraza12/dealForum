d068b32dc5c18b98e7b16e11ca9dfd983a3f1b60
jaffar raza <jaffaraza@gmail.com>
2020-09-10 15:16:39 +0500

    Validation

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -216,7 +216,7 @@ class CustomerController extends Controller
     }
 
     private function validateNumber($phone){
-        $re = '/^([0]|[+])(\d{10}|\d{12})$/m';
+        $re = '/^([0]|[+])(\d{9}|\d{10}|\d{12})$/m';
 
         if(preg_match($re,$phone)) {
             return true;
